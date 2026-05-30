0edff5dd59d2ba497ebb00cf4fbe2deb27be7006
CanC-Code <cardscollectiblesandoddities@gmail.com>
2026-05-30 15:59:43 -0600

    Update pokedex_species.asm

--- text/pokedex_species.asm
+++ text/pokedex_species.asm
@@ -1,98 +1,98 @@
 MonSpeciesNames: ; 0x29fa6
-	dex_species "SEED"       ; SPECIES_SEED
-	dex_species "LIZARD"     ; SPECIES_LIZARD
-	dex_species "FLAME"      ; SPECIES_FLAME
-	dex_species "TINYTURTLE" ; SPECIES_TINYTURTLE
-	dex_species "TURTLE"     ; SPECIES_TURTLE
-	dex_species "SHELLFISH"  ; SPECIES_SHELLFISH
-	dex_species "WORM"       ; SPECIES_WORM
-	dex_species "COCOON"     ; SPECIES_COCOON
-	dex_species "BUTTERFLY"  ; SPECIES_BUTTERFLY
-	dex_species "HAIRY BUG"  ; SPECIES_HAIRY_BUG
-	dex_species "POISON BEE" ; SPECIES_POISON_BEE
-	dex_species "TINY BIRD"  ; SPECIES_TINY_BIRD
-	dex_species "BIRD"       ; SPECIES_BIRD
-	dex_species "MOUSE"      ; SPECIES_MOUSE
-	dex_species "BEAK"       ; SPECIES_BEAK
-	dex_species "SNAKE"      ; SPECIES_SNAKE
-	dex_species "COBRA"      ; SPECIES_COBRA
-	dex_species "POISON PIN" ; SPECIES_POISON_PIN
-	dex_species "DRILL"      ; SPECIES_DRILL
-	dex_species "FAIRY"      ; SPECIES_FAIRY
-	dex_species "FOX"        ; SPECIES_FOX
-	dex_species "BALLOON"    ; SPECIES_BALLOON
-	dex_species "BAT"        ; SPECIES_BAT
-	dex_species "WEED"       ; SPECIES_WEED
-	dex_species "FLOWER"     ; SPECIES_FLOWER
-	dex_species "MUSHROOM"   ; SPECIES_MUSHROOM
-	dex_species "INSECT"     ; SPECIES_INSECT
-	dex_species "POISONMOTH" ; SPECIES_POISONMOTH
-	dex_species "MOLE"       ; SPECIES_MOLE
-	dex_species "SCRATCHCAT" ; SPECIES_SCRATCHCAT
-	dex_species "CLASSY CAT" ; SPECIES_CLASSY_CAT
-	dex_species "DUCK"       ; SPECIES_DUCK
-	dex_species "PIG MONKEY" ; SPECIES_PIG_MONKEY
-	dex_species "PUPPY"      ; SPECIES_PUPPY
-	dex_species "LEGENDARY"  ; SPECIES_LEGENDARY
-	dex_species "TADPOLE"    ; SPECIES_TADPOLE
-	dex_species "PSI"        ; SPECIES_PSI
-	dex_species "SUPERPOWER" ; SPECIES_SUPERPOWER
-	dex_species "FLYCATCHER" ; SPECIES_FLYCATCHER
-	dex_species "JELLYFISH"  ; SPECIES_JELLYFISH
-	dex_species "ROCK"       ; SPECIES_ROCK
-	dex_species "MEGATON"    ; SPECIES_MEGATON
-	dex_species "FIRE HORSE" ; SPECIES_FIRE_HORSE
-	dex_species "DOPEY"      ; SPECIES_DOPEY
-	dex_species "HERMITCRAB" ; SPECIES_HERMITCRAB
-	dex_species "MAGNET"     ; SPECIES_MAGNET
-	dex_species "WILD DUCK"  ; SPECIES_WILD_DUCK
-	dex_species "TWIN BIRD"  ; SPECIES_TWIN_BIRD
-	dex_species "TRIPLEBIRD" ; SPECIES_TRIPLEBIRD
-	dex_species "SEA LION"   ; SPECIES_SEA_LION
-	dex_species "SLUDGE"     ; SPECIES_SLUDGE
-	dex_species "BIVALVE"    ; SPECIES_BIVALVE
-	dex_species "GAS"        ; SPECIES_GAS
-	dex_species "SHADOW"     ; SPECIES_SHADOW
-	dex_species "ROCK SNAKE" ; SPECIES_ROCK_SNAKE
-	dex_species "HYPNOSIS"   ; SPECIES_HYPNOSIS
-	dex_species "RIVER CRAB" ; SPECIES_RIVER_CRAB
-	dex_species "PINCER"     ; SPECIES_PINCER
-	dex_species "BALL"       ; SPECIES_BALL
-	dex_species "EGG"        ; SPECIES_EGG
-	dex_species "COCONUT"    ; SPECIES_COCONUT
-	dex_species "LONELY"     ; SPECIES_LONELY
-	dex_species "BONEKEEPER" ; SPECIES_BONEKEEPER
-	dex_species "KICKING"    ; SPECIES_KICKING
-	dex_species "PUNCHING"   ; SPECIES_PUNCHING
-	dex_species "LICKING"    ; SPECIES_LICKING
-	dex_species "POISON GAS" ; SPECIES_POISON_GAS
-	dex_species "SPIKES"     ; SPECIES_SPIKES
-	dex_species "VINE"       ; SPECIES_VINE
-	dex_species "PARENT"     ; SPECIES_PARENT
-	dex_species "DRAGON"     ; SPECIES_DRAGON
-	dex_species "GOLDFISH"   ; SPECIES_GOLDFISH
-	dex_species "STARSHAPE"  ; SPECIES_STARSHAPE
-	dex_species "MYSTERIOUS" ; SPECIES_MYSTERIOUS
-	dex_species "BARRIER"    ; SPECIES_BARRIER
-	dex_species "MANTIS"     ; SPECIES_MANTIS
-	dex_species "HUMANSHAPE" ; SPECIES_HUMANSHAPE
-	dex_species "ELECTRIC"   ; SPECIES_ELECTRIC
-	dex_species "SPITFIRE"   ; SPECIES_SPITFIRE
-	dex_species "STAGBEETLE" ; SPECIES_STAGBEETLE
-	dex_species "WILD BULL"  ; SPECIES_WILD_BULL
-	dex_species "FISH"       ; SPECIES_FISH
-	dex_species "ATROCIOUS"  ; SPECIES_ATROCIOUS
-	dex_species "TRANSPORT"  ; SPECIES_TRANSPORT
-	dex_species "TRANSFORM"  ; SPECIES_TRANSFORM
-	dex_species "EVOLUTION"  ; SPECIES_EVOLUTION
-	dex_species "BUBBLE JET" ; SPECIES_BUBBLE_JET
-	dex_species "LIGHTNING"  ; SPECIES_LIGHTNING
-	dex_species "FLAME"      ; SPECIES_FLAME_2
-	dex_species "VIRTUAL"    ; SPECIES_VIRTUAL
-	dex_species "SPIRAL"     ; SPECIES_SPIRAL
-	dex_species "FOSSIL"     ; SPECIES_FOSSIL
-	dex_species "SLEEPING"   ; SPECIES_SLEEPING
-	dex_species "FREEZE"     ; SPECIES_FREEZE
-	dex_species "GENETIC"    ; SPECIES_GENETIC
-	dex_species "NEW SPECIE" ; SPECIES_NEW_SPECIE
-	dex_species "RAT"        ; SPECIES_RAT
+        dex_species "SEED"       ; SPECIES_SEED
+        dex_species "LIZARD"     ; SPECIES_LIZARD
+        dex_species "FLAME"      ; SPECIES_FLAME
+        dex_species "TINYTURTLE" ; SPECIES_TINYTURTLE
+        dex_species "TURTLE"     ; SPECIES_TURTLE
+        dex_species "SHELLFISH"  ; SPECIES_SHELLFISH
+        dex_species "WORM"       ; SPECIES_WORM
+        dex_species "COCOON"     ; SPECIES_COCOON
+        dex_species "BUTTERFLY"  ; SPECIES_BUTTERFLY
+        dex_species "HAIRY BUG"  ; SPECIES_HAIRY_BUG
+        dex_species "POISON BEE" ; SPECIES_POISON_BEE
+        dex_species "TINY BIRD"  ; SPECIES_TINY_BIRD
+        dex_species "BIRD"       ; SPECIES_BIRD
+        dex_species "MOUSE"      ; SPECIES_MOUSE
+        dex_species "BEAK"       ; SPECIES_BEAK
+        dex_species "SNAKE"      ; SPECIES_SNAKE
+        dex_species "COBRA"      ; SPECIES_COBRA
+        dex_species "POISON PIN" ; SPECIES_POISON_PIN
+        dex_species "DRILL"      ; SPECIES_DRILL
+        dex_species "FAIRY"      ; SPECIES_FAIRY
+        dex_species "FOX"        ; SPECIES_FOX
+        dex_species "BALLOON"    ; SPECIES_BALLOON
+        dex_species "BAT"        ; SPECIES_BAT
+        dex_species "WEED"       ; SPECIES_WEED
+        dex_species "FLOWER"     ; SPECIES_FLOWER
+        dex_species "MUSHROOM"   ; SPECIES_MUSHROOM
+        dex_species "INSECT"     ; SPECIES_INSECT
+        dex_species "POISONMOTH" ; SPECIES_POISONMOTH
+        dex_species "MOLE"       ; SPECIES_MOLE
+        dex_species "SCRATCHCAT" ; SPECIES_SCRATCHCAT
+        dex_species "CLASSY CAT" ; SPECIES_CLASSY_CAT
+        dex_species "DUCK"       ; SPECIES_DUCK
+        dex_species "PIG MONKEY" ; SPECIES_PIG_MONKEY
+        dex_species "PUPPY"      ; SPECIES_PUPPY
+        dex_species "LEGENDARY"  ; SPECIES_LEGENDARY
+        dex_species "TADPOLE"    ; SPECIES_TADPOLE
+        dex_species "PSI"        ; SPECIES_PSI
+        dex_species "SUPERPOWER" ; SPECIES_SUPERPOWER
+        dex_species "FLYCATCHER" ; SPECIES_FLYCATCHER
+        dex_species "JELLYFISH"  ; SPECIES_JELLYFISH
+        dex_species "ROCK"       ; SPECIES_ROCK
+        dex_species "MEGATON"    ; SPECIES_MEGATON
+        dex_species "FIRE HORSE" ; SPECIES_FIRE_HORSE
+        dex_species "DOPEY"      ; SPECIES_DOPEY
+        dex_species "HERMITCRAB" ; SPECIES_HERMITCRAB
+        dex_species "MAGNET"     ; SPECIES_MAGNET
+        dex_species "WILD DUCK"  ; SPECIES_WILD_DUCK
+        dex_species "TWIN BIRD"  ; SPECIES_TWIN_BIRD
+        dex_species "TRIPLEBIRD" ; SPECIES_TRIPLEBIRD
+        dex_species "SEA LION"   ; SPECIES_SEA_LION
+        dex_species "SLUDGE"     ; SPECIES_SLUDGE
+        dex_species "BIVALVE"    ; SPECIES_BIVALVE
+        dex_species "GAS"        ; SPECIES_GAS
+        dex_species "SHADOW"     ; SPECIES_SHADOW
+        dex_species "ROCK SNAKE" ; SPECIES_ROCK_SNAKE
+        dex_species "HYPNOSIS"   ; SPECIES_HYPNOSIS
+        dex_species "RIVER CRAB" ; SPECIES_RIVER_CRAB
+        dex_species "PINCER"     ; SPECIES_PINCER
+        dex_species "BALL"       ; SPECIES_BALL
+        dex_species "EGG"        ; SPECIES_EGG
+        dex_species "COCONUT"    ; SPECIES_COCONUT
+        dex_species "LONELY"     ; SPECIES_LONELY
+        dex_species "BONEKEEPER" ; SPECIES_BONEKEEPER
+        dex_species "KICKING"    ; SPECIES_KICKING
+        dex_species "PUNCHING"   ; SPECIES_PUNCHING
+        dex_species "LICKING"    ; SPECIES_LICKING
+        dex_species "POISON GAS" ; SPECIES_POISON_GAS
+        dex_species "SPIKES"     ; SPECIES_SPIKES
+        dex_species "VINE"       ; SPECIES_VINE
+        dex_species "PARENT"     ; SPECIES_PARENT
+        dex_species "DRAGON"     ; SPECIES_DRAGON
+        dex_species "GOLDFISH"   ; SPECIES_GOLDFISH
+        dex_species "STARSHAPE"  ; SPECIES_STARSHAPE
+        dex_species "MYSTERIOUS" ; SPECIES_MYSTERIOUS
+        dex_species "BARRIER"    ; SPECIES_BARRIER
+        dex_species "MANTIS"     ; SPECIES_MANTIS
+        dex_species "HUMANSHAPE" ; SPECIES_HUMANSHAPE
+        dex_species "ELECTRIC"   ; SPECIES_ELECTRIC
+        dex_species "SPITFIRE"   ; SPECIES_SPITFIRE
+        dex_species "STAGBEETLE" ; SPECIES_STAGBEETLE
+        dex_species "WILD BULL"  ; SPECIES_WILD_BULL
+        dex_species "FISH"       ; SPECIES_FISH
+        dex_species "ATROCIOUS"  ; SPECIES_ATROCIOUS
+        dex_species "TRANSPORT"  ; SPECIES_TRANSPORT
+        dex_species "TRANSFORM"  ; SPECIES_TRANSFORM
+        dex_species "EVOLUTION"  ; SPECIES_EVOLUTION
+        dex_species "BUBBLE JET" ; SPECIES_BUBBLE_JET
+        dex_species "LIGHTNING"  ; SPECIES_LIGHTNING
+        dex_species "FLAME"      ; SPECIES_FLAME_2
+        dex_species "VIRTUAL"    ; SPECIES_VIRTUAL
+        dex_species "SPIRAL"     ; SPECIES_SPIRAL
+        dex_species "FOSSIL"     ; SPECIES_FOSSIL
+        dex_species "SLEEPING"   ; SPECIES_SLEEPING
+        dex_species "FREEZE"     ; SPECIES_FREEZE
+        dex_species "GENETIC"    ; SPECIES_GENETIC
+        dex_species "NEW SPECIE" ; SPECIES_NEW_SPECIE
+        dex_species "RAT"        ; SPECIES_RAT
